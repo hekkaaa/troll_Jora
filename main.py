e26f675e994d87b4ec223bbebf4651fbca618081
Andrey Kalkutin <CosmoStar@protonmail.com>
2021-07-23 13:18:13 +0500

    Custom images search added

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import vk_api
 import hashlib
 
-YANDEX_SEARCH = 'https://yandex.ru/images/search?from=tabbar&nomisspell=1&text=%D0%BF%D1%80%D0%B0%D0%B2%D0%BE%D1%81%D0%BB%D0%B0%D0%B2%D0%BD%D1%8B%D0%B5%20%D0%BE%D1%82%D0%BA%D1%80%D1%8B%D1%82%D0%BA%D0%B8%20%D1%81%20%D0%BD%D0%B0%D0%B4%D0%BF%D0%B8%D1%81%D1%8F%D0%BC%D0%B8&source=related-0'
+search_text = input('Введите текст для поиска открытки.\n\
+Например: православные открытки с надписями\nИскать: ')
+
+YANDEX_SEARCH = f'https://yandex.ru/images/search?from=tabbar&nomisspell=1&text={search_text}&source=related-0'
 
 print("Введите ваш логин VK")
 LOGIN = input()
