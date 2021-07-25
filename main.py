d1d0d9ee78cbcb159c9f0aed93bdcab6bc9e269f
Alexander Pototsky <46771781+hekkaaa@users.noreply.github.com>
2021-07-25 16:37:30 +0600

    Revert "Вынес некоторые блоки в функции, удалил хэширование"

--- main.py
+++ main.py
@@ -5,27 +5,7 @@ from bs4 import BeautifulSoup
 import hashlib
 
 
-def handle_image_link(image_link: str) -> str:
-    # Выделение ссылки на источник картинки из Яндекс-ссылки
-
-    # Получение индексов начала и конца ссылки на источник картинки
-    start = image_link.find('img_url=') + len('img_url=')
-    end = image_link[start:].find('&') + start
-
-    # Обрезка ссылки для получения ссылки на источник
-    image_link = image_link[start:end]
-
-    # Дополнительная проверка        
-    if image_link.find('?') != -1:
-        end = image_link.find('?')
-        image_link = image_link[:end]
-
-    return image_link
-
-
 def parse_images(search_text: str) -> str or None:
-    # Получение картинок по запросу и возврат ссылки на одну случайную
-    
     # Адрес запроса картинок
     query = 'https://yandex.ru/images/search'
 
@@ -39,8 +19,10 @@ def parse_images(search_text: str) -> str or None:
     # Отправка GET запроса на сервер
     response = requests.get(query, params=params)
 
-    if 'captcha' in response.text:
-        print('Ошибка - Яндекс запрашивает капчу. Попробуйте позже')
+    if response.status_code == 200:
+        print('Картинки получены')
+    else:
+        print(f'Не могу получить картинки по запросу {search_text}')
         return None
 
     soup = BeautifulSoup(response.content, "html.parser")
@@ -48,15 +30,19 @@ def parse_images(search_text: str) -> str or None:
     if not agg:
         print('Ошибка - вероятно яндекс ссылка опять дико тупит. Попробуй еще раз')
         return None
-    
-    # Выбор случайной картинки из полученного ответа
-    image_link = ''
-    while image_link[-4:] not in ['.jpg', '.png', '.bmp', '.gif']:
+    else:
+        # Выбор случайной картинки из полученного ответа
         random_number = random.randint(0, len(agg) - 1)
         image_link = requests.utils.unquote(agg[random_number]['href'])
-        image_link = handle_image_link(image_link)
 
-    return image_link
+        # Получение индексов начала и конца ссылки на источник картинки
+        start = image_link.find('img_url=') + len('img_url=')
+        end = image_link[start:].find('&') + start
+
+        # Обрезка ссылки для получения ссылки на источник
+        image_link = image_link[start:end]
+
+        return image_link
 
 
 def download_image(image_url: str) -> str:
@@ -74,75 +60,77 @@ def download_image(image_url: str) -> str:
     return filepath
 
 
-def post_vk(vk, search_query: str, user_ids: list, message: str) -> bool:
+def post_vk(login: str, password: str, search_query: str, user_ids: list, message: str) -> bool:
     # Авторизация Вконтакте
-
-    # Поиск картинок и выбор одной случайной
-    image_url = parse_images(search_query)
-        
-    # Загрузка картинки в директорию images
-    try:
-        filepath = download_image(image_url)
-        print('Картинка успешно загружена в папку images')
-    except Exception:
-        print('Не удалось загрузить картинку')
-        return False
-
-    # Загрузка картинки на сервера ВКонтакте
-    upload = vk_api.VkUpload(vk)
-    try:
-        photo = upload.photo_wall(filepath)
-    except Exception:
-        print('Не удалось загрузить картинку на сервера ВКонтакте')
-        return False
-
-    # Получение данных картинки из ответа VK API
-    owner_id = photo[0]['owner_id']
-    photo_id = photo[0]['id']
-    access_key = photo[0]['access_key']
-
-    # Создание ВК-ссылки на картинку
-    attachment = f'photo{owner_id}_{photo_id}_{access_key}'
-
-    # Постинг картинки каждому юзеру из user_ids
-    for user_id in user_ids:
-        try:
-            vk.wall.post(message=message,
-                         attachments=attachment, owner_id=user_id)
-            print(f'Картинка отправлена на стену пользователя {user_id}!')
-        except vk_api.exceptions.ApiError as ex:
-            print(f"\nError! Ошибка ID '{user_id}':")
-            print(ex)
-        except Exception:
-            print("Неизвестная ошибка :(")
-            
-    return True
-
-
-def auth_vk(login: str, password: str) -> vk_api.vk_api.VkApiMethod:
-    # Авторизация Вконтакте
-    
     vk_session = vk_api.VkApi(login, password)
     vk_session.auth()
 
-    return vk_session.get_api()
+    vk = vk_session.get_api()
+
+    # Поиск картинок и выбор одной случайной
+    image_url = parse_images(search_query)
+
+    if not image_url:
+        print("Не взлетело")
+        return False
+    else:
+        # Отправка картинки на стену пользователю.
+
+        # Загрузка картинки в директорию images
+        try:
+            filepath = download_image(image_url)
+            print('Картинка успешно загружена в папку images')
+        except Exception:
+            print('Не удалось загрузить картинку')
+            return False
+
+        # Загрузка картинки на сервера ВКонтакте
+        upload = vk_api.VkUpload(vk)
+
+        # Здесь просходит ошибка [100]
+        # vk_api.exceptions.ApiError: [100] One of the parameters specified was missing or invalid: photos_list is invalid
+        # ВК не нравится картинка.
+        try:
+            photo = upload.photo_wall(filepath)
+        except vk_api.exceptions.ApiError as ex:
+            print("Ошибка загрузки картинки. Попробуйте еще раз")
+            print(ex)
+            return False
+        except:
+            print("Неизвестная ошибка :(")
+            return True
+
+        # Получение данных картинки из ответа VK API
+        owner_id = photo[0]['owner_id']
+        photo_id = photo[0]['id']
+        access_key = photo[0]['access_key']
+
+        # Создание ВК-ссылки на картинку
+        attachment = f'photo{owner_id}_{photo_id}_{access_key}'
+
+        # Постинг картинки каждому юзеру из user_ids
+        for user_id in user_ids:
+            try:
+                vk.wall.post(message=message,
+                             attachments=attachment, owner_id=user_id)
+                print(f'Картинка отправлена на стену пользователя {user_id}!')
+            except vk_api.exceptions.ApiError as ex:
+                print(f"\nError! Ошибка ID '{user_id}':")
+                print(ex)
+            except Exception:
+                print("Неизвестная ошибка :(")
+    return True
 
 
 if __name__ == "__main__":
 
-    while True:
-        login = input("Введите ваш логин VK: ")
-        password = input("Введите ваш пароль: ")
+    login = input("Введите ваш логин VK: ")
+    password = input("Введите ваш пароль: ")
 
-        try:
-            vk = auth_vk(login, password)
-            print('Авторизация ВКонтакте прошла успешно!')
-            break
-        except vk_api.exceptions.BadPassword:
-            print('Неверный пароль. Попробуйте еще раз.')
-        except Exception as ex:
-            print('Не удалось пройти авторизацию. Возникла ошибка:', ex)
-    
+    ## хеширование пароля
+    hash_object = hashlib.sha256(password.encode())
+    password = hash_object.hexdigest()
+
     while True:
         print('Введите текст для поиска открытки. Например: "православные открытки с надписями"\n')
         search_text = input('Искать: ')
@@ -156,7 +144,7 @@ if __name__ == "__main__":
         if not message:
             message = 'Sent with python script'
 
-        post_vk(vk, search_text, user_ids, message)
+        post_vk(login, password, search_text, user_ids, message)
 
         print("Рассылка закончена")
         print("=" * 40 + '\n')
